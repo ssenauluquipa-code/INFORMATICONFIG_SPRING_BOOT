662141a407dd0a5afe2de774c2b0b5a27ce8a9d4
senauluquipas <senauluquipas@gmail.com>
2025-09-04 00:31:53 -0400

    se agrego el DTO

--- src/main/java/sena/springbootapp/controllers/EjemploControllers.java
+++ src/main/java/sena/springbootapp/controllers/EjemploControllers.java
@@ -1,13 +1,17 @@
 package sena.springbootapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EjemploControllers {
 	
 	@GetMapping("detalle_info")
-	public String info() {
+	public String info(Model modal) {
+		modal.addAttribute("Titulo", "Servidor");
+		modal.addAttribute("Servidor", "Samuel Servidor");
+		modal.addAttribute("IP", "192.168.0.1");
 		return "detalle_info";
 	}
 
